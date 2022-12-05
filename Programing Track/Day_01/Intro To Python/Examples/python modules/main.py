bd01bea29ef608a5c2fae95ee1fc3209eb07f4c3
MajidRaimi <imajid2000z1@gmil.com>
2022-12-05 17:52:41 +0300

    0.0.1

---
+++ Programing Track/Day_01/Intro To Python/Examples/python modules/main.py
@@ -0,0 +1,5 @@
+import operations
+print(operations.add(2, 3))
+print(operations.subtract(5, 3))
+print(operations.multiply(2, 3))
+print(operations.divide(6, 3))
---
+++ Programing Track/Day_01/Intro To Python/Examples/python modules/operations/__init__.py
@@ -0,0 +1,4 @@
+from .addition import addition as add
+from .subtraction import subtract as subtract
+from .multiplication import multiply as multiply
+from .division import division as divide
